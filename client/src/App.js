932656f6a85fbb89fbea5c0570aa7d3d439fa30e
MinimalistCat2019 <48767239+MinimalistCat2019@users.noreply.github.com>
2020-08-13 18:45:12 +0100

    create public and private routes using hocs

--- client/src/App.js
+++ client/src/App.js
@@ -6,16 +6,18 @@ import { BrowserRouter as Router, Route} from 'react-router-dom';
 import Register from './Components/Register';
 import Todos from './Components/Todos';
 import Admin from './Components/Admin';
+import PrivateRoute from './hocs/PrivateRoute';
+import PublicRoute from './hocs/PublicRoute';
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Route exact path="/" component={Home}/>
-      <Route path='/login' component={Login} />
-      <Route path='/register' component={Register} />
-      <Route path='/todos' component={Todos} />
-      <Route path='/admin' component={Admin} />
+      <PublicRoute path='/login' component={Login} />
+      <PublicRoute path='/register' component={Register} />
+      <PrivateRoute path='/todos' component={Todos} roles={["user", "admin"]}/>
+      <PrivateRoute path='/admin' component={Admin} roles={["admin"]}/>
     </Router>
   );
 }
