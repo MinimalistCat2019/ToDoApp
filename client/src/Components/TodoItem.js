42bc5c8cf2fc956a5097e310c5b84b5a9d82d525
MinimalistCat2019 <48767239+MinimalistCat2019@users.noreply.github.com>
2020-08-18 17:55:58 +0100

    create remove todo functionality client side

--- client/src/Components/TodoItem.js
+++ client/src/Components/TodoItem.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
+import styled from 'styled-components';
+import { theme } from '../theme';
+import TodoService from '../Services/TodoService';
+import Message from '../Components/Message';
+
 
 const TodoItem = props => {
+    const [message, setMessage] = useState(null);
+
+    const onSubmit = e => {
+        e.preventDefault();
+        TodoService.removeTodo(props.todo.name)
+        .then((data) => {
+            const { message } = data;
+            if (!message.msg) {
+                setMessage(message);
+          } else if(message.msgBody === "Todo could not be removed at this time") {
+              setMessage(message);
+          } else {
+              setMessage(message)
+          }
+        });
+    };
+
     return (
-        <li>{props.todo.name}</li>
+        <>
+            <Todo>{props.todo.name}</Todo>
+            <form onSubmit={onSubmit}>
+                <button type="submit">Remove</button>
+            </form>
+        </>
     )
 }
 
 export default TodoItem;
+
+const Todo = styled.h3`
+
+`
--- client/src/Services/TodoService.js
+++ client/src/Services/TodoService.js
@@ -9,6 +9,22 @@ export default {
                     }
                 });
     },
+    removeTodo: todo => {
+        return fetch('user/todos', {
+            method: "delete",
+            body: JSON.stringify(todo.name),
+            headers: {
+                'Content-Type': "application/json"
+            }
+        })
+        .then(response => {
+            if(response.status !==401) {
+                return {message: {msgBody: "Todo successfully removed"}, msgError: false}
+            } else {
+            return {message: {msgBody: "Todo could not be removed at this time"}, msgError: true}
+            }
+        });
+    },
     postTodo: todo => {
         return fetch('/user/todo', {
             method: "post",
