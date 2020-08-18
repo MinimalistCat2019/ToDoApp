d368750da6365e7c3577237e15989dbdd01fa33e
MinimalistCat2019 <48767239+MinimalistCat2019@users.noreply.github.com>
2020-08-18 15:58:30 +0100

    add styling to Login component

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -2,6 +2,8 @@ import React, {useState, useContext } from 'react';
 import AuthService from '../Services/AuthService';
 import Message from '../Components/Message';
 import {AuthContext} from '../Context/AuthContext';
+import styled from 'styled-components';
+import { theme } from '../theme';
 
 const Login = props => {
     const [user, setUser] = useState({username: "", password: ""});
@@ -28,27 +30,77 @@ const Login = props => {
     }
 
     return (
-        <div>
+        <Form>
             <form onSubmit={onSubmit}> 
                 <h3>Please sign in</h3>
-                <label htmlFor="username" className="sr-only">Username: </label>
-                <input  type="text" 
-                        name="username" 
-                        onChange={onChange} 
-                        className="form-control" 
-                        placeholder="Enter Username"/>
-                <label htmlFor="password" className="sr-only">Password: </label>
-                <input  type="password" 
-                        name="password" 
-                        onChange={onChange} 
-                        className="form-control" 
-                        placeholder="Enter Password"/>
-                <button className="btn btn-lg btn-primary btn-block" 
-                        type="submit">Log in</button>
+                <br/>
+                <label htmlFor="username" className="sr-only">
+                    <Input  type="text" 
+                            name="username" 
+                            onChange={onChange} 
+                            className="form-control" 
+                            placeholder="Enter Username"/>
+                </label>
+                <label htmlFor="password" className="sr-only">
+                    <Input  type="password" 
+                            name="password" 
+                            onChange={onChange} 
+                            className="form-control" 
+                            placeholder="Enter Password"/>
+                </label>
+                <Button className="btn btn-lg btn-primary btn-block" 
+                        type="submit">Log in</Button>
             </form>
             {message ? <Message message={message}/> : null}
-        </div>
+        </Form>
     )
 }
 
 export default Login;
+
+const Form = styled.div`
+    background: ${theme.primaryLight};
+    color: ${theme.primaryDark};
+    max-width: 120rem;
+    border-radius: 15px;
+    margin: 15px;
+    padding: 2rem 2rem;
+    text-transform: uppercase;
+    *, *:before, *:after {
+        box-sizing: border-box;
+      }
+`
+const Input = styled.input.attrs(props => ({
+    // we can define static props
+  }))`
+    color: ${ theme.primaryDark };
+   
+    font-size: 1em;
+    border: 2px solid ${theme.primaryDark};
+    border-radius: 10px;
+  
+    /* here we use the dynamically computed prop */
+    margin: 0.5rem;
+    padding: 0.5rem;
+  `;
+  
+
+const Button = styled.button`
+  background: ${theme.primaryDark};
+  color: ${ theme.primaryLight };
+  font-size: 1em;
+  margin: .5em;
+  padding: 0.5rem;
+  border: 2px solid ${theme.primaryLight};
+  border-radius: 10px;
+  text-transform: uppercase;
+  &:hover {
+    text-decoration: none;
+    color: ${theme.primaryHover};
+    }
+//   @media (max-width: 768px) {
+//       font-size: .65em;
+//       padding: .25em .2em;
+//       margin: 0.2em;
+//     }
+`;
